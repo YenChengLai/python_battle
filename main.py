53cbc134e1d044282b05ed6d630fe4b23eb80fcc
Frank Lai <frank810618@gmail.com>
2022-04-24 18:16:00 -0700

    adjust minor error and add MP attack feature

--- main.py
+++ main.py
@@ -21,10 +21,38 @@ while (running):
     if index == 0:
         dmg = player.generate_damage()
         enemy.take_damage(dmg)
-        print("You attacked for", dmg, "points of damage. Enemy HP:", enemy.get_hp())
+        print("You attacked for", dmg, "points of damage.")
+    elif index == 1:
+        player.choose_magic()
+        magic_choice = int(input("Choose magic:")) - 1
+        magic_dmg = player.generate_spell_damage(magic_choice)
+        spell = player.get_spell_name(magic_choice)
+        cost = player.get_spell_mp_cost(magic_choice)
+
+        current_mp = player.get_mp()
+
+        if cost > current_mp:
+            print(bColor.FAIL + "\nNot enough MP\n" + bColor.ENDC)
+            continue
+
+        player.reduce_mp(cost)
+        enemy.take_damage(magic_dmg)
+        print(bColor.OKBLUE + "\n" + spell + " deals", str(magic_dmg), "points of damage" + bColor.ENDC)
 
     enemy_choice = 1
 
     enemy_dmg = enemy.generate_damage()
     player.take_damage(enemy_dmg)
-    print("Enemy attacks for", enemy_dmg, "Player HP", player.get_hp())
+    print("Enemy attacks for", enemy_dmg)
+
+    print("--------------------------------")
+    print("Enemy HP:", bColor.FAIL + str(enemy.get_hp()) + "/" + str(enemy.get_max_hp()) + bColor.ENDC + "\n")
+    print("Your HP:", bColor.OKGREEN + str(player.get_hp()) + "/" + str(player.get_max_hp()) + bColor.ENDC)
+    print("Your MP:", bColor.OKBLUE + str(player.get_mp()) + "/" + str(player.get_max_mp()) + bColor.ENDC + "\n")
+
+    if enemy.get_hp() == 0:
+        print(bColor.OKGREEN + "You win!" + bColor.ENDC)
+        running = False
+    elif player.get_hp() == 0:
+        print(bColor.FAIL + "Your enemy has defeated you!" + bColor.ENDC)
+        running = False
